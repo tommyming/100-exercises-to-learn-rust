9f93edc5c89fecbc36eb718d4a2449c1a19c197c
Tommy Han <tommyhonym@gmail.com>
2024-11-11 21:09:16 +0800

    finish chapter 2

--- exercises/02_basic_calculator/08_overflow/src/lib.rs
+++ exercises/02_basic_calculator/08_overflow/src/lib.rs
@@ -5,10 +5,11 @@
 // For reasons that we'll explain later, the customization needs to be done in the `Cargo.toml`
 // at the root of the repository, not in the `Cargo.toml` of the exercise.
 
+
 pub fn factorial(n: u32) -> u32 {
-    let mut result = 1;
+    let mut result: u32 = 1;
     for i in 1..=n {
-        result *= i;
+        result = result.saturating_mul(i);
     }
     result
 }
